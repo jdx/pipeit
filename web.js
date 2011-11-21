3b5c8912d1d30ce20b590de2af10d4fc51f7eb87
Jeff Dickey <dickeytk@gmail.com>
2011-11-21 13:20:16 -0800

    added redis functionality

--- web.js
+++ web.js
@@ -2,11 +2,20 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+// use $REDIS_URL or redis://localhost:6379
+var redis = require('redis-url').createClient();
+
+redis.set('status', 'working!');
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+    redis.get('status', function(err, redis_status) {
+        response.writeHead(200, { 'Content-Type': 'text/plain' });
+        response.write('Hello world!\nRedis is: ' + redis_status);
+        response.end();
+    });
 });
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 4000;
 app.listen(port, function() {
-  console.log('Listening on ' + port);
+    console.log('Listening on ' + port);
 });
