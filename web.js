7159e153297d8932152c2ba4dd8b66089e9edb31
Jeff Dickey <dickeytk@gmail.com>
2011-11-21 22:31:17 -0800

    initial boilerplate

--- web.js
+++ web.js
@@ -1,18 +1,13 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
-
-// use $REDIS_URL or redis://localhost:6379
 var redis = require('redis-url').createClient();
 
-redis.set('status', 'working!');
+app.set('view engine', 'jade');
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-    redis.get('status', function(err, redis_status) {
-        response.writeHead(200, { 'Content-Type': 'text/plain' });
-        response.write('Hello world!\nRedis is: ' + redis_status);
-        response.end();
-    });
+    response.render('index');
 });
 
 var port = process.env.PORT || 4000;
