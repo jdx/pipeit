3b2edaf8dc52a935637130062b179839f99b4b40
Jeff Dickey <dickeytk@gmail.com>
2011-11-22 00:18:38 -0800

    commenting

--- public/main.js
+++ public/main.js
@@ -15,18 +15,32 @@ $(document).ready(function() {
         e.stopPropagation();
         e.preventDefault();
 
+        // Get variables to start reading the file
         var file = e.dataTransfer.files[0];
         var fileReader = new FileReader();
-        var blob = file.webkitSlice(0, BLOB_SIZE);
         var position = 0;
+
+        // Read the first blob
+        // TODO: Make this work in FF
+        var blob = file.webkitSlice(0, BLOB_SIZE);
         fileReader.readAsBinaryString(blob);
+
+        // Fired after each blob
         fileReader.onload = (function(file) {
             return function(e) {
+                // just print to console for now
                 console.log(e.target.result);
+
+                // Update the current position in the file
                 position = position + e.loaded;
+
+                // Update progress diablog
                 $('#progress').text(Math.round((position / file.size) * 100) + '%');
-                var blob = file.webkitSlice(position, position + BLOB_SIZE);
+
+                // If the file isn't done
                 if (e.loaded != 0) {
+                    // Get the next blob
+                    var blob = file.webkitSlice(position, position + BLOB_SIZE);
                     fileReader.readAsText(blob);
                 }
             };
